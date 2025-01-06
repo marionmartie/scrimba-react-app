5384c53f6e79fe1cc69051d0366708be9a5a8384
Marion Balba <balba.marion@gmail.com>
2025-01-06 13:51:53 +0800

    Changed to callback function

--- App.jsx
+++ App.jsx
@@ -3,16 +3,11 @@ import {useState} from "react"
 export default function App() {
     const [count, setCount] = useState(0)
     const increment = () => {
-        setCount( count + 1 )        
+        setCount( prevCount => prevCount + 1 )        
     }
     const decrement = () => {
-        setCount(count - 1)
+        setCount( prevCount => prevCount - 1 )        
     }
-    /**
-     * Challenge: 
-     * Create state to track our count value (initial value is 0)
-     * Don't forget to replace the hard-coded "0" with your new state
-     */
     return (
         <main className="container">
             <h1>How many times will Bob say "state" in this section?</h1>
