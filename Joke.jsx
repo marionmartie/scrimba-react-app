dcb1f3942a04f24939e6418058a8add3d499f3e1
Marion Balba <balba.marion@gmail.com>
2025-01-08 09:34:53 +0800

    Created conditional rendering

---
+++ Joke.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+export default function Joke(props) {
+    /**
+     * Challenge:
+     * - Create state `isShown` (boolean, default to `false`)
+     * - Add a button that toggles the value back and forth
+     */
+
+    const [isShown, setIsShown] = useState('false')
+
+    const showEl = () => {
+        setIsShown((prev) => !prev)
+        console.log(isShown);
+
+    }
+    return (
+        <div>
+            {props.setup && <h3>{props.setup}</h3>}
+            <button onClick={showEl}>Show</button>
+            { !isShown && <p>{props.punchline}</p>}
+            <hr />
+        </div>
+    )
+}
