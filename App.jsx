c3ccc91a0f434ac734b2a0e36c7132658aace41b
Marion Balba <balba.marion@gmail.com>
2025-01-10 09:09:20 +0800

    Changed to component

--- App.jsx
+++ App.jsx
@@ -1,20 +1,47 @@
-import {useState} from "react"
+import React from "react"
+import Count from "./Count"
 
 export default function App() {
-    const [count, setCount] = useState(0)
-    const increment = () => {
-        setCount( prevCount => prevCount + 1 )        
+    const [count, setCount] = React.useState(0)
+
+    function add() {
+        setCount(prevCount => prevCount + 1)
     }
-    const decrement = () => {
-        setCount( prevCount => prevCount - 1 )        
+
+    function subtract() {
+        setCount(prevCount => prevCount - 1)
     }
+    
+    /**
+     * Challenge:
+     * - Create a new component called `Count`
+     *    - It should receive a prop called `number`, whose value
+     *      is the current value of our count
+     *    - Have the component render the h2.count element below
+     *      and display the incoming prop `number`
+     * - Replace the h2.count below with an instance of
+     *   the new Count component, passing the correct value
+     *   to its `number` prop.
+     * - After doing this, everything should be working the
+     *   same as before.
+     */
+
     return (
         <main className="container">
-            <h1>How many times will Bob say "state" in this section?</h1>
             <div className="counter">
-                <button onClick={decrement} className="minus" aria-label="Decrease count">–</button>
-                <h2 className="count">{ count }</h2>
-                <button onClick={increment} className="plus" aria-label="Increase count">+</button>
+                <button
+                    className="minus"
+                    onClick={subtract}
+                    aria-label="Decrease count"
+                >-</button>
+
+                <Count number={count} />
+
+                <button
+                    className="plus"
+                    onClick={add}
+                    aria-label="Increase count"
+                >+</button>
             </div>
         </main>
     )
---
+++ Count.jsx
@@ -0,0 +1,8 @@
+
+const Count = ({number}) => {
+    return (
+        <h2 className="count">{number}</h2>
+    )
+}
+
+export default Count
