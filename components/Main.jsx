8ea47c130e3677c4cb9ebfde59519b908c1b70e2
Marion Balba <balba.marion@gmail.com>
2025-01-16 10:55:19 +0800

    Fetch for all memes

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function Main() {
     const [meme, setMeme] = useState({
@@ -6,21 +6,39 @@ export default function Main() {
         bottomText: "Walk into Mordor",
         imageUrl: "http://i.imgflip.com/1bij.jpg"
     })
+
+    const [allMemes, setAllMemes] = useState([])
+    
+    /**
+     * Challenge:
+     * Get an array of memes from the imgflip API as soon as
+     * this component renders for the first time.
+     * Check the imgflip documentation for the correct URL.
+     * Save the array of memes (not the whole response
+     * data) to state. (For this app, we'll randomly choose
+     * one of the memes from this array when the user clicks
+     * the "Get a new meme image" button, but we'll do that in
+     * a separate challenge.)
+     * 
+     * Hint: for now, don't try to use an async/await function.
+     * Instead, use `.then()` to resolve the promises
+     * from using `fetch`. We'll learn why after this challenge.
+     */
+    
+    useEffect( () => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+
+            console.log(allMemes)
+    }, [])
     
     function handleChange(event) {
-        const {value} = event.currentTarget
-        /**
-         * Challenge: update the topText value in the meme state
-         * object every time the topText input box is changed
-         * 
-         * Note: don't worry about bottomText at this point.
-         */
-        setMeme( prevState => {
-            return {
-                ...prevState,
-                topText: value
-            }})
-        
+        const {value, name} = event.currentTarget
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
     }
 
     return (
@@ -32,6 +50,7 @@ export default function Main() {
                         placeholder="One does not simply"
                         name="topText"
                         onChange={handleChange}
+                        value={meme.topText}
                     />
                 </label>
 
@@ -40,6 +59,8 @@ export default function Main() {
                         type="text"
                         placeholder="Walk into Mordor"
                         name="bottomText"
+                        onChange={handleChange}
+                        value={meme.bottomText}
                     />
                 </label>
                 <button>Get a new meme image 🖼</button>
