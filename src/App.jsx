16c6c1605924280c9b642e6005ae0821ba1304a4
Marion Balba <balba.marion@gmail.com>
2025-01-23 07:58:46 +0800

    Added focus change when gameWon is true

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useRef, useEffect } from "react"
 import Die from "./Die"
 import { nanoid } from "nanoid"
 
@@ -8,12 +8,16 @@ import ReactConfetti from "react-confetti"
 export default function App() {
   let [dice, setDice] = useState(generateNewDice())
   const {width, height} = useWindowSize()
+
+  const ngbutton = useRef(null)
+
   
   function generateNewDice() {
     return new Array(10)
     .fill(0)
     .map(() => ({
       'value' : Math.ceil(Math.random() * 6), 
+      // 'value': 5,
       'isHeld': false,
       'id': nanoid()
       }))
@@ -37,7 +41,9 @@ export default function App() {
             die
     ))
 
-    const gameWon = dice.every(die => die.isHeld) && dice.every(die => die.value === dice[0].value)
+  const gameWon = dice.every(die => die.isHeld) && dice.every(die => die.value === dice[0].value)
+
+  useEffect(() =>  ngbutton.current.focus() ,[gameWon])
   
   const diceElements = dice.map( num =>  <Die key={num.id} number={num.value} isHeld={num.isHeld} handlechange={changeIsHeld} id={num.id} /> )
 
@@ -49,10 +55,11 @@ export default function App() {
       <div className="dice-container py-12 grid grid-cols-5 gap-4">
         { diceElements }
       </div>
-      <button className="rounded bg-blue-600 text-white p-4 w-full" onClick={gameWon ? newGame : reroll}>{ gameWon ? 'New Game' : 'Generate New Dice' }</button>
+      <button className="rounded bg-blue-600 text-white p-4 w-full" ref={ngbutton} onClick={gameWon ? newGame : reroll}>{ gameWon ? 'New Game' : 'Generate New Dice' }</button>
       {
         gameWon && <ReactConfetti width={width} height={height} />
       }
+      <div className="" aria-live="polite">{ gameWon && <p className="sr-only">Congratulations! You won! Press "New Game" to start again.</p> }</div>
     </main>
   )
 }
--- src/Die.jsx
+++ src/Die.jsx
@@ -2,6 +2,8 @@
 const Die = ({number, isHeld, handlechange, id}) => {
     return (
         <button 
+            aria-label={`Die with label ${number}, ${isHeld ? 'held' : 'not held'}`}
+            aria-pressed={isHeld}
             onClick={() => handlechange(id)}
             className={`rounded-xl font-bold text-2xl py-2 px-2 shadow-lg ${isHeld ? 'bg-green-500' : 'bg-white'}`}>{number}</button>
     )
